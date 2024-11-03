c23c0210566cf8f1d4fa7c59d982d78198db63f8
sosislick <femishodipo2006@gmail.com>
2024-11-03 14:28:04 -0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+Python 3.13.0b1 (tags/v3.13.0b1:2268289, May  8 2024, 12:20:07) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello world")
+Hello world
